a5364dd4564457e8d024bdabcfda96949647d66e
chxxwn <lisa9902@naver.com>
2024-04-18 21:00:24 +0900

    짝수와 홀수

--- Programmers/다리를지나는트럭.js
+++ Programmers/다리를지나는트럭.js
@@ -1,56 +1,51 @@
-
-
-class Queue{
-    constructor(){
-        this.items = {}
-        this.headIndex = 0
-        this.tailIndex = 0
-    }
-    enqueue(item){
-        this.items[this.tailIndex] = item;
-        this.tailIndex++;
-    }
-    dequeue(){
-        const item = this.items[this.headIndex]
-        delete this.items[this.headIndex]
-        this.headIndex++
-        return item;
-    }
-    peek(){
-        return this.items[this.headIndex]
-    }
-    getLength(){
-        return this.tailIndex - this.headIndex
-    }
-
+class Queue {
+  constructor() {
+    this.items = {}
+    this.headIndex = 0
+    this.tailIndex = 0
+  }
+  enqueue(item) {
+    this.items[this.tailIndex] = item
+    this.tailIndex++
+  }
+  dequeue() {
+    const item = this.items[this.headIndex]
+    delete this.items[this.headIndex]
+    this.headIndex++
+    return item
+  }
+  peek() {
+    return this.items[this.headIndex]
+  }
+  getLength() {
+    return this.tailIndex - this.headIndex
+  }
 }
 
 function solution(bridge_length, weight, truck_weights) {
-    
-    let time = 0;
+  let time = 0
 
-    let bridge = Array.from({length: bridge_length}, () => 0);
-    let bridge_sum = 0;
+  let bridge = Array.from({ length: bridge_length }, () => 0)
+  let bridge_sum = 0
 
+  time += 1
+  bridge.shift()
+  bridge_sum += truck_weights[0]
+  bridge.push(truck_weights.shift())
+
+  while (bridge_sum > 0) {
     time += 1
-    bridge.shift()
-    bridge_sum += truck_weights[0]
-    bridge.push(truck_weights.shift())
+    bridge_sum -= bridge.shift()
 
-    while (bridge_sum > 0){
-
-        time += 1;
-        bridge_sum -= bridge.shift()
-
-        if(truck_weights.length > 0 && bridge_sum + truck_weights[0] <= weight){
-            bridge_sum += truck_weights[0]
-            bridge.push(truck_weights.shift())
-        }else{
-            bridge.push(0)
-        }
+    if (truck_weights.length > 0 && bridge_sum + truck_weights[0] <= weight) {
+      bridge_sum += truck_weights[0]
+      bridge.push(truck_weights.shift())
+    } else {
+      bridge.push(0)
     }
+  }
 
-    return time;
+  return time
 }
 
-console.log(solution(100, 100, [10,10,10,10,10,10,10,10,10,10]))
+console.log(solution(100, 100, [10, 10, 10, 10, 10, 10, 10, 10, 10, 10]))
--- Programmers/제곱근판별.js
+++ Programmers/제곱근판별.js
@@ -1,6 +1,6 @@
 function solution(n) {
-    const sqrt = Math.sqrt(n)
-    return (Number.isInteger(sqrt) === false) ? -1 : (sqrt+1)**2
+  const sqrt = Math.sqrt(n)
+  return Number.isInteger(sqrt) === false ? -1 : (sqrt + 1) ** 2
 }
 
 console.log(solution(121))
