1bee913561a1c0e4aaffddb12656b598fe14bcfb
chxxwn <lisa9902@naver.com>
2024-04-23 20:08:58 +0900

    콜라츠추측

---
+++ Programmers/콜라츠추측.js
@@ -0,0 +1,13 @@
+function solution(num) {
+  let count = 0
+  while (num != 1) {
+    count += 1
+    if (count == 500) {
+      return -1
+    }
+    num = num % 2 === 0 ? num / 2 : num * 3 + 1
+  }
+  return count
+}
+
+console.log(solution(626331))
