b500e9936ebdeb9a86045455f05362aeadc8f522
chxxwn <lisa9902@naver.com>
2024-05-11 20:03:02 +0900

    최소직사각형

--- Programmers/최소직사각형.js
+++ Programmers/최소직사각형.js
@@ -28,3 +28,15 @@ console.log(
     [7, 11],
   ])
 )
+
+function solution2(sizes) {
+  const rotated = sizes.map(([w, h]) => (w < h ? [h, w] : [w, h]))
+
+  let maxSize = [0, 0]
+  rotated.forEach(([w, h]) => {
+    if (w > maxSize[0]) maxSize[0] = w
+    if (h > maxSize[1]) maxSize[1] = h
+  })
+
+  return maxSize[0] * maxSize[1]
+}
